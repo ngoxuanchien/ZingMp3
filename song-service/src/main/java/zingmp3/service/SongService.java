fb2bf77395cea735df192f53ddd7385f9c2f8d6d
ngoxuanchien <74176040+ngoxuanchien@users.noreply.github.com>
2024-04-04 03:53:39 +0700

    Add song service

---
+++ song-service/src/main/java/zingmp3/service/SongService.java
@@ -0,0 +1,58 @@
+package zingmp3.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import zingmp3.dto.SongDto;
+import zingmp3.model.Song;
+import zingmp3.repository.SongRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class SongService {
+    private final SongRepository songRepository;
+
+    public Song getSong(Integer songId) {
+        return songRepository.findById(songId).orElseThrow();
+    }
+
+    public List<Song> getAllSong() {
+        return songRepository.findAll();
+    }
+
+    public Song addNewSong(SongDto request) {
+        Song song = Song.builder()
+                .songName(request.getSongName())
+                .songWriter(request.getSongWriter())
+                .thumbnail(request.getThumbnail())
+                .lyric(request.getLyric())
+                .duration(request.getDuration())
+                .providedBy(request.getProvidedBy())
+                .liked(request.getLiked())
+                .played(request.getPlayed())
+                .build();
+        return songRepository.save(song);
+
+    }
+
+    public Song updateSong(Integer songId, SongDto request) {
+        Song song = songRepository.findById(songId).orElseThrow();
+        song.setSongName(request.getSongName());
+        song.setDuration(request.getDuration());
+        song.setSongWriter(request.getSongWriter());
+        song.setThumbnail(request.getThumbnail());
+        song.setProvidedBy(request.getProvidedBy());
+        song.setLyric(request.getLyric());
+        song.setLiked(request.getLiked());
+        song.setPlayed(request.getPlayed());
+
+        return songRepository.save(song);
+    }
+
+    public void deleteSongInService(Integer songId) {
+        Song song = songRepository.findById(songId).orElseThrow();
+        songRepository.delete(song);
+    }
+}
