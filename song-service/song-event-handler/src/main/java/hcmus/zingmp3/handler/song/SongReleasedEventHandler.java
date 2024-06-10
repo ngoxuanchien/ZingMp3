6854c87fcba691602e1577fafee175fd7f7f3cf1
ngoxuanchien <74176040+ngoxuanchien@users.noreply.github.com>
2024-06-11 00:16:16 +0700

    fix song-service

---
+++ song-service/song-event-handler/src/main/java/hcmus/zingmp3/handler/song/SongReleasedEventHandler.java
@@ -0,0 +1,28 @@
+package hcmus.zingmp3.handler.song;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import hcmus.zingmp3.common.domain.model.Song;
+import hcmus.zingmp3.common.events.song.SongApprovedEvent;
+import hcmus.zingmp3.common.events.song.SongCreateEvent;
+import hcmus.zingmp3.handler.EventHandler;
+import hcmus.zingmp3.service.song.SongService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component("SONG_APPROVED")
+@RequiredArgsConstructor
+public class SongApprovedEventHandler implements EventHandler {
+    private final SongService songService;
+    private final Gson gson;
+    @Override
+    public void handle(JsonObject json) {
+        SongApprovedEvent event = gson.fromJson(
+                json,
+                SongApprovedEvent.class
+        );
+
+        Song song = gson.fromJson(gson.toJsonTree(event.getPayload()), Song.class);
+        songService.create(song);
+    }
+}
